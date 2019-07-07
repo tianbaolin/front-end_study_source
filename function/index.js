5087285d5d2d0162efd306df4e0c44d75955cbad
田宝林 <baolintian@whu.edu.cn>
2019-07-08 01:08:24 +0800

    commit

---
+++ function/index.js
@@ -0,0 +1,227 @@
+// if(true){
+//   function say(){
+//     console.log(1)
+//   }
+// }else{
+//   function say() {
+//     console.log(2)
+//   }
+// }
+// say()
+// function a (params) {
+//   const var1 = 1
+//   return function b(params) {
+//     let var2 = var1+1
+//     console.log('object :', {object:var2});
+//     return function c(params) {
+//       let var3 = var2+1
+//       return var3
+//     }
+//   }
+// }
+// let var4 = 4
+// let d = a()()
+// console.log({functionc:d})
+// let a = 1;
+// let d = null
+// {
+//   let b = a+1;
+//   let e = 5
+//     d =function (){
+//       let c = b+1
+//       let f = function(){
+//         let g = c+1
+//         return g
+//       }
+//       return f;
+//     }
+// }
+// console.log('d', {d:d()})
+// function People(){
+//   let name = 'tbl'
+//   let say = function (params) {
+//     console.log('params', params)
+//   }
+//   this.fun = function(){
+//     say()
+//   }
+// }
+// let people = new People()
+// console.log('people', people)
+// console.log('people.fun', people.fun)
+// function People() {
+// }
+// (function a() {
+//   const name = 'tbl';
+//   People.prototype.say = function b() {
+//     console.log('name', name);
+//   };
+// }());
+// const people = new People();
+// console.log('people', people);
+
+// const a = {
+//   fun() {
+//     console.log('this', this);
+//     const fun1 = () => {
+//       console.log('this', this);
+//     };
+//     fun1();
+//   },
+// };
+// a.fun();
+
+// const multipy = (() => {
+//   const cache = {};
+//   const calculate = (...params) => {
+//     let res = 1;
+//     for (let index = 0; index < params.length; index += 1) {
+//       res *= params[index];
+//     }
+//     return res;
+//   };
+//   return (...params) => {
+//     const param = params.join(',');
+//     if (param in cache) {
+//       console.log('缓存', 'cache');
+//       return cache[param];
+//     }
+//     const res = calculate(...params);
+//     cache[param] = res;
+//     return res;
+//   };
+// })();
+// multipy(10, 3, 4);
+// multipy(10, 3, 2);
+// multipy(10, 3, 6);
+// console.log('object', { multipy });
+// const type = Object.prototype.toString.call(Object.prototype);
+// console.log('object', type);
+// function calculate(x, y, z) {
+//   return x * y * z;
+// }
+// function currying(fun) {
+//   // 原始函数参数个数
+//   const { length } = fun;
+//   // 参数缓存
+//   const params = [];
+//   function middleFun(param) {
+//     params.push(param);
+//     // 参数是否全部就位
+//     if (params.length < length) {
+//       return middleFun;
+//     }
+//     return fun(...params);
+//   }
+//   return middleFun;
+// }
+// const curry = currying(calculate);
+// console.log('object', curry(6)(9)(4));
+
+// const arr = [1, 2, 3, 4];
+// console.log('arr', arr);
+// arr.shift();
+// console.log('object', arr);
+// arr.shift();
+// console.log('object', arr);
+// arr.shift();
+// console.log('object', arr);
+
+// const arr = [1, 2, 3, 4];
+// Function.prototype.uncurrying = function uncurrying() {
+//   // 函数对象
+//   const self = this;
+//   return function middleFun(...params) {
+//     for (const item of params) {
+//       self.call(this, item);
+//     }
+//     return this;
+//   };
+// };
+// arr.uncurry = arr.push.uncurrying();
+// arr.uncurry('1', '2', '3', '4');
+// console.log('arr :', arr);
+// function throttle(fun, time) {
+//   let first = true;
+//   let timer = null;
+//   return function middle(...params) {
+//     // 第一次或者已经过了一定时间
+//     if (first === true || timer === null) {
+//       fun.call(this, ...params);
+//       first = false;
+//       timer = window.setTimeout(() => {
+//         timer = null;
+//       }, time);
+//     }
+//   };
+// }
+// function handler(event) {
+//   console.log('this', this);
+//   console.log('event', event);
+// }
+// document.onmousemove = throttle(handler, 1000);
+// window.onclick = function name(event) {
+//   console.log('event :', event.currentTarget);
+// };
+
+// // 每组大小
+// const unit = 100;
+// // 组数
+// const size = 1000;
+// const timeDistance = 10;
+// let count = 0;
+// const timeChunk = function name() {
+//   for (let i = 1; i <= unit; i += 1) {
+//     const div = document.createElement('div');
+//     div.textContent = `hello${i + count * unit}`;
+//     document.body.appendChild(div);
+//   }
+// };
+
+// const timerId = window.setInterval(() => {
+//   if (count < size) {
+//     timeChunk();
+//     count += 1;
+//   } else {
+//     window.clearInterval(timerId);
+//   }
+// }, timeDistance);
+
+// let getPlatform = () => {
+//   // 首次计算
+//   const { userAgent } = navigator;
+//   const res = userAgent.toUpperCase().indexOf('MAC OS');
+//   const result = res >= 0;
+//   // 后续计算，直接返回结果
+//   getPlatform = () => result;
+//   return result;
+// };
+// console.log('object', getPlatform());
+// console.log('object', getPlatform());
+
+
+function memorize(fun) {
+  const cache = {};
+  return (...params) => {
+    const sign = params.join(',');
+    // console.count(sign);
+    if (sign in cache) {
+      return cache[sign];
+    }
+    const res = fun(...params);
+    cache[sign] = res;
+    return res;
+  };
+}
+const memorizeAdd = memorize((n) => {
+  console.count('fibola');
+  if (n < 2) {
+    return n;
+  }
+  return memorizeAdd(n - 1) + memorizeAdd(n - 2);
+});
+
+
+console.log('object', 'start');
+console.log('object', memorizeAdd(44));
+console.log('object', memorizeAdd(44));
--- inherit/index2.js
+++ inherit/index2.js
@@ -1,12 +1,12 @@
-function People(name,age){
+function People(name, age) {
   this.name = name;
   this.age = age;
 
-  if(typeof this.say !== 'function'){
-    this.say = function(){
+  if (typeof this.say !== 'function') {
+    this.say = function say() {
       console.log('object :', 'hello');
-    }
+    };
   }
 }
-let people = new People('tian baolin',27)
-console.log('people', people)
+const people = new People('tian baolin', 27);
+console.log('people', people);
